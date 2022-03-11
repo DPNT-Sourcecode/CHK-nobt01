ef05478bd5af5aa5438318881a78ff1c783f8136
sbx_user1051 <sbx_user1051@169.254.34.117>
2022-03-11 16:17:28 +0000

    Fri Mar 11 16:17:28 UTC 2022

--- lib/send_command_to_server.py
+++ lib/send_command_to_server.py
@@ -14,7 +14,7 @@ from runner.user_input_action import get_user_input
 
 """
   ~~~~~~~~~~ Running the system: ~~~~~~~~~~~~~
- de
+
     From IDE:
        Run this file from the IDE.
  
@@ -71,4 +71,5 @@ ChallengeSession\
     .with_config(Utils.get_config())\
     .with_action_provider(lambda: get_user_input(sys.argv[1:]))\
     .start()
+
 
