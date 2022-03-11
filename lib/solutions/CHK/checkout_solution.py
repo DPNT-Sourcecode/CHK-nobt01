7ca5217bed7f98d9328a6cdc94abdf875a64c26b
sbx_user1051 <sbx_user1051@169.254.34.117>
2022-03-11 16:42:52 +0000

    Fri Mar 11 16:42:52 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,7 +2,7 @@ from collections import Counter
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
+def checkout(skus: str) -> int:
 
     price_table = {
         'A': [50, (3,150)], 
@@ -14,8 +14,11 @@ def checkout(skus):
 
     for item in basket:
         if item not in price_table:
-            print('-1')
+            return -1
+        if isinstance(price_table[item], list):
+            print(price_table[item])
 
 checkout('ABCDBCF')
+
 
 
