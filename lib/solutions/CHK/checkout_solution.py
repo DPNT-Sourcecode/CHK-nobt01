531ed6e6ea0aa04842ca63d3ad3290b2f797d0ce
sbx_user1051 <sbx_user1051@169.254.34.117>
2022-03-11 17:20:50 +0000

    Fri Mar 11 17:20:50 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,11 +5,11 @@ from collections import Counter
 def checkout(skus: str) -> int:
 
     price_table = {
-        'A': [50, (3,20), (5,50)], 
-        'B': [30, (2,45)],
-        'C': 20,
-        'D': 15,
-        'E': [40, (2,30)]}
+        'A': (50, True), 
+        'B': (30, True),
+        'C': (20, False),
+        'D': (15, False),
+        'E': (40, True)}
 
     special_offers = {
         'A': [(3,20), (5,50)],
@@ -23,17 +23,23 @@ def checkout(skus: str) -> int:
     for item in basket:
         if item not in price_table:
             return -1
-        if isinstance(price_table[item], list):
+        price, any_special_offer = price_table[item]
+        if any_special_offer:
             price, special_offer = price_table[item]
             count, offer_price = special_offer
             total_price += offer_price * (basket[item] // count)
             total_price += price * (basket[item] % count)
         else:
-            total_price += price_table[item] * basket[item]
+            total_price += calculate_price(price_table[item], basket[item])
 
     return int(total_price)
 
-def calculate_special_offers(item, basket, price_table):
-    
+def calculate_price(price, count):
+    return price * count
+
+def calculate_special_offers(item, special_offers, price_table):
+    if len(special_offers[item]) == 1:
+        item_pack, discount = special_offers[item]
+
 
 
